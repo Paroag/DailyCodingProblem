63c0d8a2f350566331ce5d75c62de8370d5ce839
Oscar  Barbier <oscarbarbier@MBP-de-Oscar.capfigroup.domain.com>
2019-11-13 17:18:01 +0100

    update Quora

--- Quora09112019_34.py
+++ Quora09112019_34.py
@@ -15,18 +15,33 @@ As another example, given the string "google", you should return "elgoogle".
 # Solution Oscar
 # Not completed, I could not find any solution to this problem ...
 
+def extract_common_char(string1, string2) :
+	return(set(string1) & set(string2))
+
+def reverse(string) :
+	return(string[::-1])
+
 def problem_solver(string) :
 
-	def is_palindrome(s) :
-		if len(s) < 2 :
-			return True
-		else :
-			return(s[0]==s[-1] and is_palindrome(s[1:-1]))
+	if len(set(string)) == len(string) :
+		return(string[::-1][:-1] + string)
+	else :
+		left_word = ""
+		right_word = ""
+		i = 0
+		while not extract_common_char(left_word, right_word) :
+			i += 1
+			left_word = string[:i]
+			right_word = string[len(string)-i:]
+		common_letter = list(extract_common_char(left_word, right_word))[0]
+		first_i = string.find(common_letter)
+		last_i = len(string) - string[::-1].find(common_letter) - 1
+		first_part = reverse(string[last_i+1:]) + string[:first_i+1]
+		last_part = reverse(first_part)
+		return(first_part+problem_solver(string[first_i+1:last_i])+last_part)
 
-
-
-	
 
 print(problem_solver("race")) #-> ecarace
 print(problem_solver("google")) #-> elgoogle
 print(problem_solver("iabcdba")) #-> iabdcdbai
+print(problem_solver("ABCAXYZDCB")) #-> ???
